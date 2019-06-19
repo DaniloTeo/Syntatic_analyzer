f919b4ff215f10506a0c7e2eda235d976e8310d3
DaniloTeo <danilo.teo@usp.br>
2019-06-19 14:26:44 -0300

    Incorporados os elementos de to_parse.py no codigo principal e criadas as estruturas para manipulacao da tokenizacao; Implementacao de obter_simbolo()

--- sth.py
+++ sth.py
@@ -1,5 +1,5 @@
 import re
-from nltk.tokenize
+from nltk.tokenize import RegexpTokenizer
 
 '''
 TO DO:
@@ -11,10 +11,6 @@ TO DO:
 '''
 
 
-LAMBDA = '%$'
-ID = '[a-zA-Z][a-zA-Z0-9]*'
-NUM_INT = '\d+'
-NUM_REAL = "\d+.\d+"
 
 
 #------------------------------------------------------------------------------------------------------------------
@@ -91,6 +87,53 @@ s_fator = ['+', '-', '*', '/']
 s_numero = [';', '*', '/', '+', '-']
 
 #------------------------------------------------------------------------------------------------------------------
+# Macros de auxilio
+LAMBDA = '%$'
+ID = '[a-zA-Z][a-zA-Z0-9]*'
+NUM_INT = '\d+'
+NUM_REAL = "\d+.\d+"
+
+# Codigo para teste
+raw = '''program nome1;
+{exemplo 1 fdsuafdsaop 3214543254 #$%}
+var a, a1, b: integer;
+begin
+read(a);
+a1:= a1*2;
+while (a1<0.00) do
+begin
+write(a1);
+a1:= a1-1;
+end;
+for b:=1 to 10 do
+begin
+b:=b+2;
+a:=a-1;
+end;
+if a<> b then write(a);
+end.'''
+
+
+
+
+
+
+
+# Expressoes regulares para gerar o tokenizador
+re_palavras_reservadas = 'program|begin|end|const|var|real|integer|procedure|if|then|else|read|write|while|do|for|to'
+re_simbolos = '>=|<=|<>|:=|=|>|<|\+|-|\*|/|\)|\(|,|\.|;|:'
+re_num_id = '\d+\.\d+|\d+|[a-zA-Z][a-zA-Z0-9]*'
+
+tok = RegexpTokenizer(re_palavras_reservadas + '|' + re_simbolos + '|' + re_num_id + '|' + re_comment)
+
+# Codigo tokenizado, sobre a qual o programa percorrera
+codigo = tok.tokenize(raw)
+
+# posicao do leitor do programa sobre o codigo
+pos = 0
+
+def obter_simbolo():
+	pos += 1
 
 def erro(esp, simb, seguidor): #????
 	print('Erro!!\ttoken "{esp}" esperado!')
